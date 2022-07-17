b2e02d6f8abcc48767645fd62ef112a49fd7cf64
maddyrae9 <madisonmulligan97@gmail.com>
2022-07-17 13:32:49 -0400

    Added product-routes

---
+++ routes/api/index.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+const productRoutes = require('./product-routes');
+const categoryRoutes = require('./category-routes');
+const tagRoutes = require('./tag-routes');
+
+router.use('/categories', categoryRoutes);
+router.use('/products', productRoutes);
+router.use('/tags', tagRoutes);
+module.exports = router;
